023e0e8963d6fd80bf6fb96ac3e3974e2ccf4696
vineethbabu <vineethbabu@users.noreply.github.com>
2016-10-22 21:01:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,15 @@
-var busno=document.getElementById("busno");
-var submit=document.getElementbyId("submit");
-submit.onclick=function {
-    
+ var map=function loadMap() {
+			
+            var mapOptions = {
+               center:new google.maps.LatLng(17.609993, 83.221436), 
+               zoom:12, 
+               mapTypeId:google.maps.MapTypeId.ROADMAP
+            };
+				
+            var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+         }
+			
+         google.maps.event.addDomListener(window, 'load', loadMap);
+      
+      
+   
