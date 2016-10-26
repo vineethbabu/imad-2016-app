ac4d3937e1f60f9f5a693c52ba29dd462b25c696
vineethbabu <vineethbabu@users.noreply.github.com>
2016-10-26 21:13:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'vineethbabu',
+    database: 'vineethbabu',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
 
+var pool = new Pool(config)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -19,6 +29,18 @@ app.get('/ui/SmartBus.png', function (req, res) {
 
 app.get('/ui/logo.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
+});
+
+app.get('/test-db', function (req, res) {
+ pool.query('SELECT*FROM dbtest',function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+         else
+         {
+             res.send(JSON.stringify(result));
+         }
+     }
+ }
 });
 
 
