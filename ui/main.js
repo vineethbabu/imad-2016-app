f3d2e2648c1c5c4f9b2f189b28d34ab77dfb7d20
vineethbabu <vineethbabu@users.noreply.github.com>
2016-11-12 23:16:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,8 @@
- var button=document.getElementById("busnumber");
- 
- 
- button.onclick=function loadMap() {
-			
-   var mapOptions = {
-               center:new google.maps.LatLng(17.609993, 83.221436), 
-               zoom:12, 
-               mapTypeId:google.maps.MapTypeId.ROADMAP
-            };
-				
-            var map = new google.maps.Map(document.getElementById("map"),mapOptions);
-         }
-			
-      
-      busnumber.innerHTML=button;
-   
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), 
+    zoom: 10
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
